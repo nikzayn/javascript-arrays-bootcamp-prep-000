94e640c810dd0e6e457b35c6f6c4d10d1b49375d
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-02-03 17:20:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,5 +27,5 @@ function destructivelyRemoveElementFromEndOfArray(array,element){
   return array;
 }
 function removeElementFromEndOfArray(array){
-  return array.slice(0, )
+  return array.slice(0, array.length.slice)
 }
